bf7de7f9ee99ed31222b719fb1d69e9efd945d04
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-19 01:58:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,8 @@
 def prime?(number)
   number = number.abs
   collection = (1..number).to_a
-  divisible = collection.any?{|num| number % num == 0 and num != number and num != 1}
 
-  if divisible? || number == 1 || number == 0
+  if collection.any?{|num| number % num == 0 and num != number and num != 1} || number == 1 || number == 0
     return false
   else
     return true
