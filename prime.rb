44a1a9d23d01fee2071b1e793c1e90a90cbc0a23
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-19 01:24:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(number)
   collection = (1..number).to_a
 
-  if collection.any?{|num| number % num == 0 } == true
+  if collection.any?{|num| number % num == 0 and num != 1} == true
   end
 end
 
